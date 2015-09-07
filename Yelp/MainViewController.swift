4cae10b7fa8883948de7b37d535a9090bff32768
admin <admin@admins-iMac.local>
2015-09-08 06:50:23 +0700

    Restructure 2nd

---
+++ Yelp/MainViewController.swift
@@ -0,0 +1,142 @@
+//
+//  MainViewController.swift
+//  Yelp
+//
+//  Created by admin on 9/5/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController, UIScrollViewDelegate, UISearchBarDelegate, FiltersViewControllerDelegate {
+
+    var businesses: [Business]!
+    var result: [Business]!
+    var searchBar:UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 200, 20))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchBar.placeholder = "Find your place"
+        self.navigationItem.titleView = searchBar
+        
+        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+        
+        // Delegate Datasource
+        searchBar.delegate = self
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        if let businesses = self.businesses {
+            return businesses.count
+        }
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("VenueCell", forIndexPath: indexPath) as! VenueCell
+        cell.initWithBusiness(self.businesses[indexPath.row])
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.endEditing(true)
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+        let navController = segue.destinationViewController as! UINavigationController
+        let filtersController = navController.topViewController as! FiltersViewController
+        filtersController.delegate = self
+        /*
+        detailViewController.item = movie
+        JTProgressHUD.show()
+
+        let detailViewController = segue.destinationViewController as! FiltersViewController
+        detailViewController.categories = self.categories
+        */
+    }
+
+    func filtersView(filterView: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+        
+        var filter = filters["categories"] as? [String]
+        
+        Business.searchWithTerm("Restaurants", sort: nil, categories: filter, deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+        
+    }
+
+    override func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        println(searchText)
+    }
+}
